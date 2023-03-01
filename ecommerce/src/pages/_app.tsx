e3c84deeaf3b34e60a58922e22fdf86d86a4360b
Abraham Avila <aavilas75@hotmail.com>
2023-03-01 16:26:18 -0600

    Instal Chakra UI and clean up

---
+++ ecommerce/src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import type { AppProps } from 'next/app'
+
+// pages/_app.js
+import { ChakraProvider } from '@chakra-ui/react'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+}
