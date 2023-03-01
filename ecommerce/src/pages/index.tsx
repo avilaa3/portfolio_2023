119498750f8bb42fdf370d5f2bc1d4c1976b0247
Abraham Avila <aavilas75@hotmail.com>
2023-03-01 17:27:44 -0600

    Render products to home page

--- ecommerce/src/pages/index.tsx
+++ ecommerce/src/pages/index.tsx
@@ -1,7 +1,26 @@
 import Head from 'next/head'
 import { Heading } from '@chakra-ui/react'
+import { GetServerSidePropsContext } from 'next';
 
-export default function Home() {
+type Product = {
+  id: number,
+  title: string,
+  price: number,
+  description: string,
+  category: string,
+  image: string,
+  rating: {
+  rate: number,
+  count: number
+  };
+}
+
+type Props = {
+  products: Product[] 
+}
+// this is executhe in the client side
+export default function Home({products}) {
+  console.log(products);
   
   return (
     <>
@@ -12,8 +31,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main >
-      <Heading>We got Chakra UI</Heading>
+        <ol>
+        {products.map(product => {
+          return <li key={product.id}>{product.title}</li>
+        })}
+        </ol>
       </main>
     </>
   )
+}
+// This happen in the server side.
+export async function getServerSideProps(contex: GetServerSidePropsContext){
+  const products = await fetch('https://fakestoreapi.com/products')
+    .then(res=>res.json())
+            
+  return{
+    props: {
+      products
+    }
+  }
 }
